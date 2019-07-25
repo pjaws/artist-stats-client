ee72808f9a595550ddd9f427c3ec52c70ee194c8
Paul Matthew Jaworski <53156002+pjaws@users.noreply.github.com>
2019-07-25 12:00:57 -0700

    refactor ArtistForm to use hooks

--- src/ArtistForm.js
+++ src/ArtistForm.js
@@ -1,54 +1,50 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class ArtistForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { artist: "" };
+async function getArtistStats(artist) {
+  const response = await fetch("/artists?q=" + artist);
+  const artistStats = await response.json();
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ artist: event.target.value });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    console.log(this.state.artist);
-    this.props.onFormSubmit();
-    this.props.setArtistData({ ...this.state });
-  }
-
-  render() {
-    return (
-      <section className="hero">
-        <div className="hero-body">
-          <div className="container">
-            <h1 className="title">Get stats on your favorite Spotify artist</h1>
-            <form onSubmit={this.handleSubmit} className="artist-search">
-              <div className="field has-addons has-addons-centered">
-                <div className="control">
-                  <input
-                    className="input"
-                    name="artist"
-                    id="artist"
-                    value={this.state.value}
-                    onChange={this.handleChange}
-                  />
-                </div>
-                <div className="control">
-                  <button type="submit" className="button is-primary">
-                    Go!
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </section>
-    );
-  }
+  return artistStats;
 }
+
+const ArtistForm = ({ onFormSubmit, setArtistData }) => {
+  const [artist, setArtist] = useState("");
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    getArtistStats(artist).then(artistData => {
+      setArtistData(artistData);
+      onFormSubmit();
+    });
+  };
+
+  return (
+    <section className="hero">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Get stats on your favorite Spotify artist</h1>
+          <form onSubmit={handleSubmit} className="artist-search">
+            <div className="field has-addons has-addons-centered">
+              <div className="control">
+                <input
+                  className="input"
+                  name="artist"
+                  id="artist"
+                  value={artist}
+                  onChange={e => setArtist(e.target.value)}
+                />
+              </div>
+              <div className="control">
+                <button type="submit" className="button is-primary">
+                  Go!
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default ArtistForm;

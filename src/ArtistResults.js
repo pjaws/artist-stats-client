41c2360e5fd652edc82407c41f2824a544ffbbf9
Paul Matthew Jaworski <53156002+pjaws@users.noreply.github.com>
2019-07-26 13:21:31 -0700

    starting to look halfway decent

--- src/ArtistResults.js
+++ src/ArtistResults.js
@@ -7,19 +7,63 @@ const ArtistResults = ({ artistData, onBackClick }) => {
     <section className="section">
       <div className="container">
         <h1 className="title is-1">Stats for {artistData.artistName}</h1>
-        <div className="level">
-          <div className="level-item has-text-centered">
-            <div>
+        <div className="columns">
+          <div className="column has-text-centered">
+            <div className="box">
               <p className="heading">Danceability</p>
               <p className="title">{artistData.danceability}</p>
             </div>
-            <div>
+          </div>
+          <div className="column has-text-centered">
+            <div className="box">
               <p className="heading">Energy</p>
               <p className="title">{artistData.energy}</p>
             </div>
-            <div>
+          </div>
+          <div className="column has-text-centered">
+            <div className="box">
               <p className="heading">Loudness</p>
               <p className="title">{artistData.loudness}</p>
+            </div>
+          </div>
+        </div>
+        <div className="columns">
+          <div className="column has-text-centered">
+            <div className="box">
+              <p className="heading">Acousticness</p>
+              <p className="title">{artistData.acousticness}</p>
+            </div>
+          </div>
+          <div className="column has-text-centered">
+            <div className="box">
+              <p className="heading">Instrumentalness</p>
+              <p className="title">{artistData.instrumentalness}</p>
+            </div>
+          </div>
+          <div className="column has-text-centered">
+            <div className="box">
+              <p className="heading">Speechiness</p>
+              <p className="title">{artistData.speechiness}</p>
+            </div>
+          </div>
+        </div>
+        <div className="columns">
+          <div className="column has-text-centered">
+            <div className="box">
+              <p className="heading">Valence</p>
+              <p className="title">{artistData.valence}</p>
+            </div>
+          </div>
+          <div className="column has-text-centered">
+            <div className="box">
+              <p className="heading">Liveness</p>
+              <p className="title">{artistData.instrumentalness}</p>
+            </div>
+          </div>
+          <div className="column has-text-centered">
+            <div className="box">
+              <p className="heading">Tempo</p>
+              <p className="title">{artistData.tempo}</p>
             </div>
           </div>
         </div>
